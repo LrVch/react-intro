773c43141e935119f6c378778ebf3727a701db44
RLonskiy <RLonskiy@luxoft.com>
2019-07-26 11:34:42 +0600

    deploy

--- src/components/UI/NotFound/NotFound.js
+++ src/components/UI/NotFound/NotFound.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const styles = {
-
+  flexGrow: 1,
+  display: 'flex',
 }
 
 const NotFound = () => {
   return (
     <div style={styles}>
-      <h1>404</h1>
+      <h1 style={{margin: 'auto'}}>404</h1>
     </div>
   )
 }
